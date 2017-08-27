7776ca69e2648efce5439a154ca6e98d58c5eac4
Rafael Andrade <30444471+RafaelAndrade0@users.noreply.github.com>
2017-08-27 14:00:05 -0300

    Updating PEP 8 Style

--- 2Questao.py
+++ 2Questao.py
@@ -8,19 +8,23 @@ def show_image(im):
     plt.imshow(image, interpolation='nearest')
     plt.show()
 
+
 def n_channels(altura, largura, nchannels):
     return nchannels
 
+
 def size(altura, largura, nchannels):
     return altura, largura
+
 
 def imreadgray(im):
     image = mpimg.imread(im)
     # Array slicing. A aplicação de uma pseudo cor só é relevante em um único canal,
     # por isso lum_img só recebe um canal. (Tanto faz o qual. R, G ou B)!
-    lum_img = image[:,:,0] 
+    lum_img = image[:, :, 0] 
     plt.imshow(lum_img, cmap="gray")
     plt.show()
+
 
 # Resulta num cinza mais escuro, em comparação a função imreadgray()
 def rgb2gray(im, altura, largura):
@@ -36,13 +40,14 @@ def rgb2gray(im, altura, largura):
 
 
 def negative(im, altura, largura):
-	for row in range(altura):
-		for col in range(largura):
-			red = 255 - im[row][col][0]
-			green = 255 - im[row][col][1]
-			blue = 255 - im[row][col][2]
-			im[row][col] = [red, green, blue]
-	return im
+    for row in range(altura):
+        for col in range(largura):
+            red = 255 - im[row][col][0]
+            green = 255 - im[row][col][1]
+            blue = 255 - im[row][col][2]
+            im[row][col] = [red, green, blue]
+    return im
+
 
 # Organizar Direito a função.
 def thresh(im, altura, largura, value):
@@ -58,49 +63,49 @@ def thresh(im, altura, largura, value):
 def main():
     path = '/home/rafael/Documentos/ProceImgs/emiliaclarke.jpg'
 
-    #Exibe a imagem em nd.array
+    # Exibe a imagem em nd.array
     img = mpimg.imread(path)
     print(img)
 
-    #Converte a imagem en nd.array para uma imagem de fato
+    # Converte a imagem en nd.array para uma imagem de fato
     imgplot = plt.imshow(img)
     plt.show(imgplot)
 
-    #Mostra a imagem original no console
-    #Recebe uma imagem como parametro e a exibe. Só isso.
-    #show_image(path)
+    # Mostra a imagem original no console
+    # Recebe uma imagem como parametro e a exibe. Só isso.
+    # show_image(path)
 
-    #Recebe uma imagem como parametro e a retorna em escala de cinza.
+    # Recebe uma imagem como parametro e a retorna em escala de cinza.
     imreadgray(path)
 
     shape = img.shape
 
-    #Numero de canais da imagem
+    # Numero de canais da imagem
     print('O número de canais da imagem é de: {0}'
           .format(n_channels(*shape)))
 
-    #Altura e Largura da imagem
+    # Altura e Largura da imagem
     altura_largura = size(*shape)
     print('A altura da imagem é de: {0} e a largura é de: {1}'
           .format(altura_largura[0], altura_largura[1]))
 
-    #negative() função ==NEGATIVA DA IMAGEM==
-    #negative_image = negative(img, altura_largura[0], altura_largura[1])
-    #imgplot_negative = plt.imshow(negative_image)
-    #plt.show(imgplot_negative)
+    # negative() função ==NEGATIVA DA IMAGEM==
+    # negative_image = negative(img, altura_largura[0], altura_largura[1])
+    # imgplot_negative = plt.imshow(negative_image)
+    # plt.show(imgplot_negative)
 
-    #Rg2toGray() função ==ESCALA DE CINZA DA IMAGEM==
+    # Rg2toGray() função ==ESCALA DE CINZA DA IMAGEM==
     grey_image = rgb2gray(img, altura_largura[0], altura_largura[1])
     print(grey_image)
     imgplot_grey = plt.imshow(grey_image)
     plt.show(imgplot_grey)
 
-    #thresh() função ==THRESHOLD DA IMAGEM==
+    # thresh() função ==THRESHOLD DA IMAGEM==
     thresh_image = thresh(img, altura_largura[0], altura_largura[1], 64)
     plt.imshow(img, cmap=plt.cm.gray)
     plt.show()
-    #imgplot_thresh = plt.imshow(thresh_image)
-    #plt.show(imgplot_thresh)
+    # imgplot_thresh = plt.imshow(thresh_image)
+    # plt.show(imgplot_thresh)
 
 if __name__ == '__main__':
     main()
